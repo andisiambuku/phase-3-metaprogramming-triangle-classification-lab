8adbe41bcd7828488926b1cc1b4b9d37945dd23d
andisiambuku <roselandambuku@gmail.com>
2022-10-25 12:33:41 +0300

    Assignment Done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,46 @@
 class Triangle
-  # write code here
+  # triangle code
+  attr_reader :side_1, :side_2, :side_3
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind
+        valid_triangle
+        
+        if side_1 == side_2 && side_2 == side_3
+          :equilateral
+          
+        
+        elsif side_1 == side_2 || side_2 == side_3 || side_1 == side_3
+          :isosceles
+        else
+        :scalene
+      end
+  end
+
+  def more_than_zero?
+    [side_1, side_2, side_3].all?(&:positive?)
+  end
+
+    def triangle_inequal?
+      side_1 + side_2 > side_3 && side_1 + side_3 > side_2 && side_2+side_3 > side_1
+    end
+
+    def valid_triangle
+      raise TriangleError unless more_than_zero? && triangle_inequal?
+    end
+
+
+
+
+
+  class TriangleError < StandardError
+    # triangle error code
+
+
+  end
 end
